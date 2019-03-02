785646724108a2ffd666c2aedcbae11ee26bbbed
Jordan Hennessy <jorrdygirl@me.com>
2019-03-02 17:01:44 -0600

    Exercise 2.3

---
+++ week-2/routes/hennessy-exercise-2.3.js
@@ -0,0 +1,29 @@
+// require statements
+var express = require('express');
+var http = require('http');
+
+var app = express();
+
+// routes
+app.get('/', function(req, res){
+  res.end('Welcome to the homepage. \n'); // setting message for homepage
+
+});
+
+app.get('/about', function(req, res){
+  res.end('Welcome to the about page. \n'); // setting message for about page
+});
+
+app.get('/contact', function(req, res){
+  res.end('Welcome to the contact page. \n'); // setting message for contact page
+});
+
+app.use(function(req, res){
+  res.statsCode = 404; // setting 404 message (none existent page)
+  res.end('404!\n');
+});
+
+// create server
+http.createServer(app).listen(3000,function(){
+  console.log('Application started on port %s', 3000);
+});
